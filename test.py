628c5db11eaea14c202c976846553b48215a03d0
Alexander Argentakis <38327951+MFDGaming@users.noreply.github.com>
2021-05-07 01:09:20 +0300

    Update test.py

--- test.py
+++ test.py
@@ -3,5 +3,5 @@ from anvil.region import region
 reg: object = region("./r.0.0.mca")
 chunk: bytes = reg.get_chunk_data(0, 4)
 reg.put_chunk_data(0, 4, chunk)
-chunk: bytes = reg.get_chunk(0, 4)
+chunk: bytes = reg.get_chunk_data(0, 4)
 print(chunk)
