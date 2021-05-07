c18ea7fbea772a60cbf4ed441fbc5360ae5fc7d6
Alexander Argentakis <38327951+MFDGaming@users.noreply.github.com>
2021-05-07 10:19:12 +0300

    Update chunk.py

--- anvil/chunk.py
+++ anvil/chunk.py
@@ -1,4 +1,5 @@
 from nbt_utils.tag.compound_tag import compound_tag
+from nbt_utils.tag.int_tag import int_tag
 from nbt_utils.utils.nbt_be_binary_stream import nbt_be_binary_stream
 
 class chunk:
@@ -7,7 +8,7 @@ class chunk:
         self.z: int = z
   
     def read_chunk_data(self, chunk_data: bytes) -> None:
-        stream: object = nbt_be_binary_stream(chunk)
+        stream: object = nbt_be_binary_stream(chunk_data)
         tag: object = compound_tag()
         tag.read(stream)
         root_tag: object = tag.read_tag("")
@@ -18,6 +19,11 @@ class chunk:
         self.data: object = level_tag
             
     def write_chunk_data(self) -> bytes:
-        stream: object = nbt_be_binary_stream(chunk)
+        stream: object = nbt_be_binary_stream()
         tag: object = compound_tag()
-        
+        tag.set_tag(compound_tag())
+        level_tag: object = self.data
+        level_tag.set_tag(int_tag("xPos", self.x)
+        level_tag.set_tag(int_tag("zPos", self.s)
+        tag.get_tag("").set_tag(level_tag)
+        tag.get_tag("").set_tag(int_tag("DataVersion", self.data_version))
