1d4aec1bfeee0884786e3d69aa5c620ada504aa4
Alexander Argentakis <38327951+MFDGaming@users.noreply.github.com>
2021-05-17 18:12:09 +0300

    Update test.py

--- test.py
+++ test.py
@@ -2,11 +2,13 @@ from anvil.region import region
 from nbt_utils.tag.compound_tag import compound_tag
 from nbt_utils.utils.nbt_be_binary_stream import nbt_be_binary_stream
 
-reg: object = region("./r.0.0.mca")
+reg: object = region("./r.-1.-3.mca")
 chunk: bytes = reg.get_chunk_data(0, 4)
 reg.put_chunk_data(0, 4, chunk)
 chunk: bytes = reg.get_chunk_data(0, 4)
 stream: object = nbt_be_binary_stream(chunk)
 tag: object = compound_tag()
 tag.read(stream)
-print(tag.get_tag("").get_tag("Level").value)
+level_tag: object = tag.get_tag("").get_tag("Level")
+print(level_tag.get_tag("xPos")
+print(level_tag.get_tag("yPos")
