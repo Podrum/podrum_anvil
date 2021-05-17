b3bf39162f2815eabc04f9dae7d5b2b91d4c789b
Alexander Argentakis <38327951+MFDGaming@users.noreply.github.com>
2021-05-17 18:29:32 +0300

    Update test.py

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ from anvil.region import region
 from nbt_utils.tag.compound_tag import compound_tag
 from nbt_utils.utils.nbt_be_binary_stream import nbt_be_binary_stream
 
-reg: object = region("./r.0.0.mca")
+reg: object = region("./r.-1.-1.mca")
 chunk: bytes = reg.get_chunk_data(0, 4)
 reg.put_chunk_data(0, 4, chunk)
 chunk: bytes = reg.get_chunk_data(0, 4)
